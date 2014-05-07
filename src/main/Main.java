758842e4100a993009ca53e783c9d8a7a9797130
tomoyashibata <wind.of.hometown@gmail.com>
2014-05-07 16:03:12 +0900

    Fixed comment, and test commit.

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,6 @@ public class Main {
 	private static void getDayDiff () {
 		TimeZone.setDefault(TimeZone.getTimeZone("JST"));
 
-		// システム現在日の0時0分0秒を取得
 		Calendar nowCal    = Calendar.getInstance();
 		int nowYear        = nowCal.get(Calendar.YEAR);
 		int nowMonth       = nowCal.get(Calendar.MONTH);
@@ -22,7 +21,6 @@ public class Main {
 		Calendar midNowCal = Calendar.getInstance();
 		midNowCal.set(nowYear, nowMonth, nowDay, 0, 0, 0);
 
-		// システム現在日の前日の0時0分0秒を取得
 		Calendar yesCal = Calendar.getInstance();
 		int billedDateYear  = yesCal.get(Calendar.YEAR);
 		int billedDateMonth = yesCal.get(Calendar.MONTH);
@@ -31,18 +29,17 @@ public class Main {
 		Calendar midYesCal = Calendar.getInstance();
 		midYesCal.set(billedDateYear, billedDateMonth, billedDateDay, 0, 0, 0);
 
-		System.out.println("nowCal     :" + nowCal.getTime());
-		System.out.println("midNowCal  :" + midNowCal.getTime());
-		System.out.println("yesCaleCal :" + yesCal.getTime());
-		System.out.println("midYesCal  :" + midYesCal.getTime());
+		System.out.println("nowCal    :" + nowCal.getTime());
+		System.out.println("midNowCal :" + midNowCal.getTime());
+		System.out.println("yesCaleCal:" + yesCal.getTime());
+		System.out.println("midYesCal :" + midYesCal.getTime());
 
-		// 2つの日付間の日数を計算するため、long値(ミリ秒）に変換
 		long timeYesCal = midYesCal.getTimeInMillis();
 		long timeNowCal = midNowCal.getTimeInMillis();
 
-		// 演算を行い、結果をミリ秒 → 秒へ変換
-		long timeDiff   = timeNowCal - timeYesCal;
-		long dayDiff = TimeUnit.MILLISECONDS.toDays(timeDiff);
+		// millseconds to day
+		long timeDiff = timeNowCal - timeYesCal;
+		long dayDiff  = TimeUnit.MILLISECONDS.toDays(timeDiff);
 		
 		System.out.println("timeDiff:" + timeDiff);
 		System.out.println("dayDiff :" + (int)dayDiff);
